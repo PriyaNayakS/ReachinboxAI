fa01eb7b186a882059a16ce4735f0904c6f1416c
PriyaNayakS <141226108+PriyaNayakS@users.noreply.github.com>
2024-09-10 22:41:00 +0530

    added the logo

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// JavaScript can handle form validation, Google authentication API integration, etc.
+document.querySelector('.google-btn').addEventListener('click', function() {
+    alert('Google sign-in functionality will go here.');
+});
+
+document.querySelector('.create-account-btn').addEventListener('click', function() {
+    alert('Account creation functionality will go here.');
+});
